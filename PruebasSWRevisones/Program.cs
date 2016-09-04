0c5c09eca7c006b36fc109d577f53a9e13626e46
erickfmm <erickfmm@gmail.com>
2016-09-04 20:25:06 -0300

    Subir Programa

---
+++ PruebasSWRevisones/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasSWRevisones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 2; int num2 = -1;
+            if(num1 > 0 && (num2 > 0)){ //Valida que ambos números sean positivos
+                Console.WriteLine("Ambos números son positivos");
+            } else
+            {
+                Console.WriteLine("Al menos un número es negativo");
+            }
+            Console.ReadLine();
+            return;
+        }
+    }
+}
